ef383a97d1cd7c1a2e3e8a4b985dc250d1690fe3
Daniel Ilies <iliesh.daniel@gmail.com>
2023-09-12 10:26:18 +0300

    trying to resolve the model saving

--- pyton/ultimate_neural_network/eval.py
+++ pyton/ultimate_neural_network/eval.py
@@ -1,5 +1,6 @@
 import os
 from matplotlib import pyplot as plt
+import tensorflow as tf
 from config import IMAGE_SIZE, BATCH_SIZE, EPOCHS
 
 def eval(model, test_generator, history, start_datetime, finish_datetime):
@@ -44,13 +45,13 @@ def eval(model, test_generator, history, start_datetime, finish_datetime):
         plt.savefig(f'/models/{current_model_folder_name}/metrics/tragining_{metric}.png', bbox_inches='tight')
 
     def saveModel(name='model'):
-        os.makedirs(f'/models/{current_model_folder_name}/saved_model/keras', exist_ok=True)
-        os.makedirs(f'/models/{current_model_folder_name}/saved_model/tf', exist_ok=True)
-        os.makedirs(f'/models/{current_model_folder_name}/saved_model/h5', exist_ok=True)
+        os.makedirs(f'/models/{current_model_folder_name}/saved_model/keras/', exist_ok=True)
+        os.makedirs(f'/models/{current_model_folder_name}/saved_model/tf/', exist_ok=True)
+        os.makedirs(f'/models/{current_model_folder_name}/saved_model/h5/', exist_ok=True)
         
-        model.save(f'/models/{current_model_folder_name}/saved_model/keras/{name}.keras', save_format='keras')
-        model.save(f'/models/{current_model_folder_name}/saved_model/tf/{name}', save_format='tf')
-        model.save(f'/models/{current_model_folder_name}/saved_model/h5/{name}.h5', save_format='h5')
+        tf.keras.saving.save_model(model, f'/models/{current_model_folder_name}/saved_model/keras/{name}.keras')
+        tf.keras.saving.save_model(model, f'/models/{current_model_folder_name}/saved_model/tf/{name}', save_format='tf')
+        tf.keras.saving.save_model(model, f'/models/{current_model_folder_name}/saved_model/h5/{name}.h5', save_format='h5')
 
     saveMetric('loss')
     saveMetric('accuracy')
