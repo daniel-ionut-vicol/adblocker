95670409365ca219f5fb115c80a40ce5eafd2ea9
Daniel Ilies <iliesh.daniel@gmail.com>
2023-09-13 14:07:01 +0300

    deleted load_model

--- pyton/ultimate_neural_network/load_model.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras.models import load_model
-from custom_metrics import precision, recall, f1_score
-
-def load_model(model_path):
-    return load_model(model_path, custom_objects={'precision': precision, "recall": recall, "f1_score": f1_score})
