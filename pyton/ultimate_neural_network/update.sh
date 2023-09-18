03734152a35da1ed708b64eb96b00737a757ba65
Daniel Ilies <iliesh.daniel@gmail.com>
2023-09-18 11:57:04 +0300

    update script

--- pyton/ultimate_neural_network/update.sh
+++ pyton/ultimate_neural_network/update.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Set Git to use the credential memory cache
-git config --global credential.helper cache
-
-# Set the cache to timeout after 1 hour (setting is in seconds)
+# Set Git to use the credential memory cache with a timeout
 git config --global credential.helper 'cache --timeout=3600'
 
-# Add your GitHub username and password/PAT to the Git configuration
-# TODO: Find another way to authenticate, this is unsecure
-git config --global user.name "RocketChamp"
-git config --global user.password "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"
-
 # Replace <TOKEN> with your actual GitHub personal access token
-git pull
+echo "https://RocketChamp:<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>@github.com" > ~/.git-credentials
 
+# Secure your ~/.git-credentials file
+chmod 600 ~/.git-credentials
+
+# Pull the latest code from the Git repository
+git pull
 
 # Step 2: remove the current container image with a specific name
 docker rm -f ad-detection-cnn || true # Replace <CONTAINER_NAME> with your actual container name
