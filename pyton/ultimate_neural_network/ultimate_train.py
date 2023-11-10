637c3d509d5ce75947390eb913a862eaf79c9c69
Daniel Ilies <iliesh.daniel@gmail.com>
2023-11-10 15:18:38 +0200

    resnet50 model training

--- pyton/ultimate_neural_network/ultimate_train.py
+++ pyton/ultimate_neural_network/ultimate_train.py
@@ -1,139 +1,100 @@
 import os
 import datetime
-import random
 import numpy as np
 import tensorflow as tf
-from sklearn import preprocessing
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.callbacks import ModelCheckpoint
-from PIL import Image
-# ---------------
-import config
-from ultimate_generator import Generator
-from resnet50_model import model
 from eval import eval
+import config
 
-def load_image_paths_labels(DATASET_PATH):
-    classes = os.listdir(DATASET_PATH)
-    lb = preprocessing.LabelBinarizer()
-    lb.fit(classes)
+# Load the pre-trained ResNet50 model without the top layer
+base_model = ResNet50(weights='imagenet', include_top=False)
 
-    image_paths = []
-    image_labels = []
+# Freeze the layers of the base model
+for layer in base_model.layers:
+    layer.trainable = False
 
-    # Get class names directly from the list of classes
-    for class_name in classes:
-        class_path = os.path.join(DATASET_PATH, class_name)
+# Add new layers for binary classification
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(1, activation='sigmoid')(x)  # Single output node for binary classification
 
-        if os.path.isdir(class_path):  # Ensure it's a directory
-            # Recursively traverse the directory structure
-            for root, _, files in os.walk(class_path):
-                for image_file_name in files:
-                    if image_file_name.endswith(".png") or image_file_name.endswith(
-                        ".jpg"
-                    ):
-                        image_path = os.path.join(root, image_file_name)
-
-                        # Attempt to open the image
-                        try:
-                            img = Image.open(image_path)
-                            img.verify()  # Verify the image file
-                        except (IOError, SyntaxError) as e:
-                            print(f"Skipping invalid image: {image_path}")
-                            continue
-
-                        image_paths.append(image_path)
-                        image_labels.append(class_name)
-
-    image_labels = np.array(lb.transform(image_labels), dtype="float32")
-
-    assert len(image_paths) == len(image_labels)
-
-    # Combine the lists into a list of tuples
-    combined = list(zip(image_paths, image_labels))
-
-    # Shuffle the combined list
-    random.shuffle(combined)
-
-    # Unzip the shuffled list back into separate lists
-    image_paths[:], image_labels[:] = zip(*combined)
-
-    return image_paths, image_labels
-
-# Load image paths and labels
-image_paths, image_labels = load_image_paths_labels(config.DATASET_PATH)
-
-# Split the data into training, validation, and test sets
-train_paths, temp_paths, train_labels, temp_labels = train_test_split(
-    image_paths, image_labels, test_size=0.2, random_state=42
-)
-val_paths, test_paths, val_labels, test_labels = train_test_split(
-    temp_paths, temp_labels, test_size=0.5, random_state=42
-)
-
-# Create generators for training, validation, and test sets
-train_generator = Generator(
-    train_paths, train_labels, BATCH_SIZE=config.BATCH_SIZE, is_training=True
-)
-val_generator = Generator(
-    val_paths, val_labels, BATCH_SIZE=config.BATCH_SIZE, is_training=False
-)
-test_generator = Generator(
-    test_paths, test_labels, BATCH_SIZE=config.BATCH_SIZE, is_training=False
-)
+model = Model(inputs=base_model.input, outputs=predictions)
 
 
-# CALLBACKS -----------------------------------
-# Define a callback for dynamic checkpoint naming and specific folder
-def get_checkpoint_callback(folder_name):
-    checkpoint_dir = os.path.join("models", folder_name, "checkpoints")
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    checkpoint_callback = ModelCheckpoint(
-        filepath=os.path.join(
-            checkpoint_dir,
-            "model_epoch_{epoch:04d}_loss_{loss:.4f}_acc_{accuracy:.4f}_val_loss_{val_loss:.4f}_val_acc_{val_accuracy:.4f}.h5",
-        ),
-        save_best_only=True,
-        monitor="val_loss",  # Monitoring validation loss
-        mode="min",
-        save_weights_only=False,  # Save entire model
-        verbose=config.VERBOSE_LEVEL,
-    )
-    return checkpoint_callback
+# Compile the model with binary cross-entropy loss
+model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
 
+# Function to load and preprocess a single image
+def preprocess_image(image_path, target_size):
+    img = load_img(image_path, target_size=target_size)
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    return tf.keras.applications.resnet50.preprocess_input(img)
 
-# Define EarlyStopping callback
-early_stopping = tf.keras.callbacks.EarlyStopping(
-    monitor="val_loss",  # Metric to monitor (e.g., validation loss)
-    patience=config.PATIENCE,  # Number of epochs with no improvement after which training will be stopped
-    restore_best_weights=True,  # Restore the model weights from the epoch with the best value of the monitored metric
-)
+# Custom generator
+def custom_generator(file_paths, labels, batch_size, target_size):
+    num_samples = len(file_paths)
+    while True:  # Loop forever so the generator never terminates
+        for offset in range(0, num_samples, batch_size):
+            batch_paths = file_paths[offset:offset + batch_size]
+            batch_labels = labels[offset:offset + batch_size]
 
-# TRAINING TIME -----------------------------------
-# Create a new directory for models
-os.makedirs("models", exist_ok=True)
+            images = [preprocess_image(path, target_size) for path in batch_paths]
+            images = np.vstack(images)
+            
+            yield images, np.array(batch_labels)
+
+# Function to recursively collect all image file paths in a given directory
+def collect_image_paths(root_dir, label):
+    file_paths = []
+    labels = []
+    for subdir, dirs, files in os.walk(root_dir):
+        for file in files:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):  # Check for image file extensions
+                file_path = os.path.join(subdir, file)
+                file_paths.append(file_path)
+                labels.append(label)
+    return file_paths, labels
+
+# Collect image paths and labels
+ad_paths, ad_labels = collect_image_paths(os.path.join(config.DATASET_PATH, 'ad'), 0)  # Label for 'Ad'
+nonad_paths, nonad_labels = collect_image_paths(os.path.join(config.DATASET_PATH, 'nonAd'), 1)  # Label for 'NonAd'
+
+# Combine the paths and labels
+file_paths = ad_paths + nonad_paths
+labels = ad_labels + nonad_labels
+
+# Split the data into training and validation sets
+train_paths, val_paths, train_labels, val_labels = train_test_split(file_paths, labels, test_size=0.2, random_state=42)
+
+# Split the data into training and validation sets
+train_paths, val_paths, train_labels, val_labels = train_test_split(file_paths, labels, test_size=0.2, random_state=42)
+
+train_generator = custom_generator(train_paths, train_labels, config.BATCH_SIZE, (config.IMAGE_SIZE, config.IMAGE_SIZE))
+val_generator = custom_generator(val_paths, val_labels, config.BATCH_SIZE, (config.IMAGE_SIZE, config.IMAGE_SIZE))
+
+# Assuming 'model' is your compiled model (e.g., based on ResNet50)
+# Define the number of steps per epoch for training and validation
+train_steps = len(train_paths) // config.BATCH_SIZE
+val_steps = len(val_paths) // config.BATCH_SIZE
 
 start_datetime = datetime.datetime.now()
-current_model_folder_name = f'model_{start_datetime.strftime("%Y-%m-%d_%H-%M-%S")}'
 
-# FIT THE MODEL
-tensorboard_callback = tf.keras.callbacks.TensorBoard(
-    log_dir=f"models/{current_model_folder_name}/logs"
-)
-
-# Train the model using the training generator
+# Fit the model
 history = model.fit(
     train_generator,
-    epochs=config.EPOCHS,  # Specify the number of training epochs
+    steps_per_epoch=train_steps,
+    epochs=config.EPOCHS,  # Number of epochs
     validation_data=val_generator,
-    verbose=config.VERBOSE_LEVEL,  # You can adjust the verbosity level
-    callbacks=[
-        tensorboard_callback,
-        get_checkpoint_callback(current_model_folder_name),
-        early_stopping,
-    ],
+    validation_steps=val_steps
 )
+
 finish_datetime = datetime.datetime.now()
 
-# EVAL AND SAVE TIME -----------------------------------
-eval(model, test_generator, history, start_datetime, finish_datetime)
+eval(model, val_generator, history, start_datetime, finish_datetime)
