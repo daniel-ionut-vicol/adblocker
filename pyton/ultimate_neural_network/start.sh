86220691fe0c9f8228a92fc3dfdc84888c538a21
Daniel Ilies <iliesh.daniel@gmail.com>
2023-11-10 13:18:32 +0200

    shell scripts

--- pyton/ultimate_neural_network/start.sh
+++ pyton/ultimate_neural_network/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# Step 4: start the docker image with a specific command
 docker run --gpus all -v ~/Documents/adblocker/data/:/app/dataset -v ~/cnn_training/models:/app/models -it ad-detection-cnn
--- pyton/ultimate_neural_network/update_jupyter.sh
+++ pyton/ultimate_neural_network/update_jupyter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Step 1: remove the current container image with a specific name
-docker rm -f ad-detection-cnn || true
-docker rmi -f ad-detection-cnn || true
+docker rm -f ad-detection-cnn-jupyter || true
+docker rmi -f ad-detection-cnn-jupyter || true
 
 # Step 2: build the docker image from current folder docker file
-docker build -t ad-detection-cnn .
+docker build -t ad-detection-cnn-jupyter .
