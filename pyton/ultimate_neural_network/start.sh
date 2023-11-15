444b49e578cd840e048120a8b07e13b5a9d0073e
Daniel Ilies <69961455+RocketChamp@users.noreply.github.com>
2023-11-15 17:07:42 +0200

    get arguments

--- pyton/ultimate_neural_network/start.sh
+++ pyton/ultimate_neural_network/start.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
-# Prompt for environment variables
-read -p "Enter image size (default 224): " IMAGE_SIZE
-IMAGE_SIZE=${IMAGE_SIZE:-224}
+# Default values
+IMAGE_SIZE=224
+BATCH_SIZE=32
+EPOCHS=10
+PATIENCE=20
+DATASET_PATH=/app/dataset
+VERBOSE_LEVEL=1
+TEXT_INPUT_SIZE=1
 
-read -p "Enter batch size (default 32): " BATCH_SIZE
-BATCH_SIZE=${BATCH_SIZE:-32}
+# A function to display a help message
+show_help() {
+    echo "Usage: $0 [options...]"
+    echo
+    echo "Options:"
+    echo " -i, --image_size       Set the image size (default: $IMAGE_SIZE)"
+    echo " -b, --batch_size       Set the batch size (default: $BATCH_SIZE)"
+    echo " -e, --epochs           Set the number of epochs (default: $EPOCHS)"
+    echo " -p, --patience         Set the patience (default: $PATIENCE)"
+    echo " -d, --dataset_path     Set the dataset path (default: $DATASET_PATH)"
+    echo " -v, --verbose_level    Set the verbosity level (default: $VERBOSE_LEVEL)"
+    echo " -t, --text_input_size  Set the text input size (default: $TEXT_INPUT_SIZE)"
+    echo " -h, --help             Display this help message"
+}
 
-read -p "Enter epochs (default 10): " EPOCHS
-EPOCHS=${EPOCHS:-10}
+# Parse command-line arguments
+OPTIONS=$(getopt -o i:b:e:p:d:v:t:h --long image_size:,batch_size:,epochs:,patience:,dataset_path:,verbose_level:,text_input_size:,help -n "$0" -- "$@")
+eval set -- "$OPTIONS"
 
-read -p "Enter patience (default 20): " PATIENCE
-PATIENCE=${PATIENCE:-20}
-
-read -p "Enter dataset path (default /app/dataset): " DATASET_PATH
-DATASET_PATH=${DATASET_PATH:-/app/dataset}
-
-read -p "Enter verbosity level (default 1): " VERBOSE_LEVEL
-VERBOSE_LEVEL=${VERBOSE_LEVEL:-1}
-
-read -p "Enter text input size (default 1): " TEXT_INPUT_SIZE
-TEXT_INPUT_SIZE=${TEXT_INPUT_SIZE:-1}
+while true; do
+    case "$1" in
+        -i|--image_size) IMAGE_SIZE=$2; shift 2;;
+        -b|--batch_size) BATCH_SIZE=$2; shift 2;;
+        -e|--epochs) EPOCHS=$2; shift 2;;
+        -p|--patience) PATIENCE=$2; shift 2;;
+        -d|--dataset_path) DATASET_PATH=$2; shift 2;;
+        -v|--verbose_level) VERBOSE_LEVEL=$2; shift 2;;
+        -t|--text_input_size) TEXT_INPUT_SIZE=$2; shift 2;;
+        -h|--help) show_help; exit 0; shift;;
+        --) shift; break;;
+        *) echo "Internal error!"; exit 1;;
+    esac
+done
 
 # Run the Docker command
 docker run --gpus all -v ~/dataset/:/app/dataset -v ~/cnn_training/models:/app/models -it -e IMAGE_SIZE=$IMAGE_SIZE -e BATCH_SIZE=$BATCH_SIZE -e EPOCHS=$EPOCHS -e PATIENCE=$PATIENCE -e DATASET_PATH=$DATASET_PATH -e VERBOSE_LEVEL=$VERBOSE_LEVEL -e TEXT_INPUT_SIZE=$TEXT_INPUT_SIZE ad-detection-cnn
