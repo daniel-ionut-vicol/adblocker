5f2ed32b245b31c88ff628e628b060c6de25bf83
Daniel Ilies <iliesh.daniel@gmail.com>
2023-11-16 17:08:18 +0200

    better prompting

--- pyton/flask_backend/clip_backend.py
+++ pyton/flask_backend/clip_backend.py
@@ -1,15 +1,19 @@
 from flask import Flask, request, jsonify
 import torch
 from transformers import CLIPProcessor, CLIPModel, CLIPImageProcessor
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 import io
 
 app = Flask(__name__)
 
 # Load a pre-trained CLIP model and processor
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch16")
-feature_extractor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch16")
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+feature_extractor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+# Move to GPU if available
+if torch.cuda.is_available():
+    model = model.to('cuda')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -20,41 +24,44 @@ def predict():
     if file.filename == '':
         return jsonify({'error': 'No file selected'}), 400
 
-    if file:
+    try:
+        # Load and resize the image
         image = Image.open(io.BytesIO(file.read()))
+        resized_image = image.resize((224, 224))  # Resize the image to 224x224
 
         # Preprocess the image
-        image_input = feature_extractor(images=image, return_tensors="pt")
+        image_input = feature_extractor(images=resized_image, return_tensors="pt")
 
-        text = ["Advertisement"]  # This should be dynamic based on your use case
+        # Define text labels
+        texts = [ "An image depicting a typical advertisement. It is characterized by vibrant colors and high-contrast lighting to catch the viewer's eye. The composition includes a clear brand logo and a product or service being prominently displayed. There's persuasive text, such as a catchy slogan or a special offer, aimed at encouraging consumer action. Elements like human models, exaggerated emotions, or idealized scenarios are common, emphasizing the product's benefits. The layout is professional, with a focus on aesthetic appeal and strategic placement of text and visuals to guide the viewer's attention.", "An image representing a non-commercial, everyday scene. It is marked by a more natural and subdued color palette, with lighting that reflects real-world conditions rather than dramatic or staged enhancements. The content is devoid of brand logos, promotional text, or sales pitches. Instead, it might feature landscapes, unposed people, animals, or common objects in their natural state, without any artificial staging. The composition is candid and unstructured, capturing a slice of life or a natural scene without the intention of selling or promoting any product or service."] # Preprocess the text
+        text_input = processor(texts, return_tensors="pt", padding=True, truncation=True)
 
-        # Preprocess the text
-        text_input = processor(text, return_tensors="pt")
-
-        # Move to GPU if available
+        # Move tensors to GPU if available
         if torch.cuda.is_available():
-            model = model.to('cuda')
             image_input = {k: v.to('cuda') for k, v in image_input.items()}
             text_input = {k: v.to('cuda') for k, v in text_input.items()}
 
         # Get the model's prediction
         with torch.no_grad():
-            image_outputs = model.get_image_features(**image_input)
-            text_outputs = model.get_text_features(**text_input)
+            image_features = model.get_image_features(**image_input)
+            text_features = model.get_text_features(**text_input)
 
-        # Normalize the features
-        image_features = torch.nn.functional.normalize(image_outputs, dim=-1)
-        text_features = torch.nn.functional.normalize(text_outputs, dim=-1)
+            # Normalize the features
+            image_features = torch.nn.functional.normalize(image_features, dim=-1)
+            text_features = torch.nn.functional.normalize(text_features, dim=-1)
 
-        # Calculate the cosine similarity between image and text features
-        cosine_similarity = torch.nn.functional.cosine_similarity(image_features, text_features).item()
+            # Calculate the cosine similarity between image and text features
+            similarities = torch.nn.functional.cosine_similarity(image_features, text_features).tolist()
 
-        # Define a threshold for classification (adjust as needed)
-        threshold = 0.5
+        # Return the similarity scores as a list
+        return jsonify(similarities)
 
-        # Make a decision based on the threshold
-        result = cosine_similarity > threshold
-        return jsonify({'is_ad': result, 'similarity': cosine_similarity})
+    except UnidentifiedImageError:
+        return jsonify({'error': 'Invalid image format'}), 400
+    except Exception as e:
+        # Log the exception details here if needed
+        return jsonify({'error': 'An error occurred processing the request'}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
--- pyton/ultimate_neural_network/start.sh
+++ pyton/ultimate_neural_network/start.sh
@@ -11,17 +11,17 @@ TEXT_INPUT_SIZE=1
 
 # A function to display a help message
 show_help() {
-    echo "Usage: $0 [options...]"
-    echo
-    echo "Options:"
-    echo " -i, --image_size       Set the image size (default: $IMAGE_SIZE)"
-    echo " -b, --batch_size       Set the batch size (default: $BATCH_SIZE)"
-    echo " -e, --epochs           Set the number of epochs (default: $EPOCHS)"
-    echo " -p, --patience         Set the patience (default: $PATIENCE)"
-    echo " -d, --dataset_path     Set the dataset path (default: $DATASET_PATH)"
-    echo " -v, --verbose_level    Set the verbosity level (default: $VERBOSE_LEVEL)"
-    echo " -t, --text_input_size  Set the text input size (default: $TEXT_INPUT_SIZE)"
-    echo " -h, --help             Display this help message"
+	echo "Usage: $0 [options...]"
+	echo
+	echo "Options:"
+	echo " -i, --image_size       Set the image size (default: $IMAGE_SIZE)"
+	echo " -b, --batch_size       Set the batch size (default: $BATCH_SIZE)"
+	echo " -e, --epochs           Set the number of epochs (default: $EPOCHS)"
+	echo " -p, --patience         Set the patience (default: $PATIENCE)"
+	echo " -d, --dataset_path     Set the dataset path (default: $DATASET_PATH)"
+	echo " -v, --verbose_level    Set the verbosity level (default: $VERBOSE_LEVEL)"
+	echo " -t, --text_input_size  Set the text input size (default: $TEXT_INPUT_SIZE)"
+	echo " -h, --help             Display this help message"
 }
 
 # Parse command-line arguments
@@ -29,19 +29,50 @@ OPTIONS=$(getopt -o i:b:e:p:d:v:t:h --long image_size:,batch_size:,epochs:,patie
 eval set -- "$OPTIONS"
 
 while true; do
-    case "$1" in
-        -i|--image_size) IMAGE_SIZE=$2; shift 2;;
-        -b|--batch_size) BATCH_SIZE=$2; shift 2;;
-        -e|--epochs) EPOCHS=$2; shift 2;;
-        -p|--patience) PATIENCE=$2; shift 2;;
-        -d|--dataset_path) DATASET_PATH=$2; shift 2;;
-        -v|--verbose_level) VERBOSE_LEVEL=$2; shift 2;;
-        -t|--text_input_size) TEXT_INPUT_SIZE=$2; shift 2;;
-        -h|--help) show_help; exit 0; shift;;
-        --) shift; break;;
-        *) echo "Internal error!"; exit 1;;
-    esac
+	case "$1" in
+	-i | --image_size)
+		IMAGE_SIZE=$2
+		shift 2
+		;;
+	-b | --batch_size)
+		BATCH_SIZE=$2
+		shift 2
+		;;
+	-e | --epochs)
+		EPOCHS=$2
+		shift 2
+		;;
+	-p | --patience)
+		PATIENCE=$2
+		shift 2
+		;;
+	-d | --dataset_path)
+		DATASET_PATH=$2
+		shift 2
+		;;
+	-v | --verbose_level)
+		VERBOSE_LEVEL=$2
+		shift 2
+		;;
+	-t | --text_input_size)
+		TEXT_INPUT_SIZE=$2
+		shift 2
+		;;
+	-h | --help)
+		show_help
+		exit 0
+		shift
+		;;
+	--)
+		shift
+		break
+		;;
+	*)
+		echo "Internal error!"
+		exit 1
+		;;
+	esac
 done
 
 # Run the Docker command
-docker run --gpus all -v ~/dataset/:/app/dataset -v ~/cnn_training/models:/app/models -it -e IMAGE_SIZE=$IMAGE_SIZE -e BATCH_SIZE=$BATCH_SIZE -e EPOCHS=$EPOCHS -e PATIENCE=$PATIENCE -e DATASET_PATH=$DATASET_PATH -e VERBOSE_LEVEL=$VERBOSE_LEVEL -e TEXT_INPUT_SIZE=$TEXT_INPUT_SIZE ad-detection-cnn
+docker run --gpus all -v ~/adblocker/pyton/ultimate_neural_network:/app -v ~/dataset:/app/dataset -v ~/cnn_training/models:/app/models -it -e IMAGE_SIZE=$IMAGE_SIZE -e BATCH_SIZE=$BATCH_SIZE -e EPOCHS=$EPOCHS -e PATIENCE=$PATIENCE -e DATASET_PATH=$DATASET_PATH -e VERBOSE_LEVEL=$VERBOSE_LEVEL -e TEXT_INPUT_SIZE=$TEXT_INPUT_SIZE ad-detection-cnn
