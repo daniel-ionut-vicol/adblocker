84b71a871c58dc7a43f870f985542aef7c392839
Daniel Ilies <iliesh.daniel@gmail.com>
2024-01-05 13:10:17 +0200

    set_memory_growth

--- pyton/ultimate_neural_network/training/resnet50_training.py
+++ pyton/ultimate_neural_network/training/resnet50_training.py
@@ -20,6 +20,9 @@ import config
 tf.keras.backend.clear_session()
 gpus = tf.config.experimental.list_physical_devices('GPU')
 
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
 strategy = tf.distribute.MirroredStrategy()
 print(f"Number of GPUs: {strategy.num_replicas_in_sync}")
 
